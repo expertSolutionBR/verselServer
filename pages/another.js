d01bb31d9e6b4dd4c6a80b42c95549e5c340c127
Denisson Silva <denisson.js@outlook.com>
2022-08-07 22:13:31 -0300

    first modifications

---
+++ pages/another.js
@@ -0,0 +1,5 @@
+function home2(){
+    return <div>Another Hello World for you</div>
+}
+
+export default home2
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function home(){
+    return <form action="./test.html">
+        <input type="submit" value="Submit"></input>
+    </form>
+}
+
+export default home
